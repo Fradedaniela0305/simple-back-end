907e2b298b8a188a92d9ee919d3c19860ddddcda
“FradeDaniela0305” <fradedaniela@yahoo.com>
2026-02-14 15:57:55 -0800

    installed express package

--- server.js
+++ server.js
@@ -2,3 +2,5 @@
 // node -v to check
 // npm -v to see if the package manager is there
 // npm init -y to initialize a node project
+// then we import Express to use its API for building your server more easily.
+// npm install express
