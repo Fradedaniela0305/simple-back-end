3e5f7dbf065e7961898928d5290a561a5d58cf7d
“FradeDaniela0305” <fradedaniela@yahoo.com>
2026-02-14 16:42:09 -0800

    set up server

--- server.js
+++ server.js
@@ -3,4 +3,49 @@
 // npm -v to see if the package manager is there
 // npm init -y to initialize a node project
 // then we import Express to use its API for building your server more easily.
-// npm install express
+// npm install express to install it
+
+// node js turns our computer into a server
+// express makes it easier to handle requests
+
+// node server.js to put the server up and running
+
+// npm run dev (after adding dev to scripts in JSON) but this youd have to rerun the server every time
+// npm install --save-dev nodemon (we install as a developer dependencies rather than jsut a dependency)
+
+// http://localhost:8383
+// http://localhost:8383
+
+const express = require('express'); // now we load express into our project
+const app = express(); // app instance
+const PORT = 8383; // define where in the ip adress the incoming requests will come
+
+
+// configure server to interpret requests
+// add HTTP verbs and routes
+
+
+app.get('/', (req,res) => {
+    // this is endpoint number 1
+    console.log("yay i hit an endpoint")
+    res.sendStatus(200); // heres our response
+
+
+}) // assume they want to get information
+// when a client makes a get request run this funciton
+// first argument is / route (the home route)
+// the arrow funciton will define waht to do when we request a get into the home route
+
+app.get('/dashboard',(req, res) => {
+    console.log("Now i got to dashboaurd");
+    res.send('hi')
+})
+
+// so we have the get http verb, defined two endopoints that use it and how we respond to them
+
+app.get('/htmlcode', (req,res)=>{
+    res.send('<h1> htlm code because why not</h1>')
+})
+
+app.listen(PORT, () => console.log(`server has started on: ${PORT}`)); // listens to incoming resquests 
+
